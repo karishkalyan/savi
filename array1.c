837336ee87d821342a7fb2dfef528d61f4cecfc5
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-24 16:57:08 +0530

    sum
    array sum

---
+++ array1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{ 
+int a[5]={1,2,3,4,5},k,i,sum=0;
+printf("\n enter the value of k:");
+scanf("%d",&k);
+for(i=0;i<k;i++);
+{
+sum=sum+n[i];
+}
+printf("\n the sum is %d",sum);
+getch();
+]
